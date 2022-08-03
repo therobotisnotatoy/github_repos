8e422a7adf74bfee868d96083cc9cb33247cad64
therobotisnotatoy <pythonshumor@gmail.com>
2022-08-03 15:21:18 +0300

    Removed trashes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,3 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-
-account = Account.create!(
-  nick_name: Faker::Name.name,
-  user_name: Faker::Name.name,
-  repos: Faker::Name.name
-)
